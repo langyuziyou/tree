65c9a66daeda5a87fb73c8ba6ed5b65be0e90655
yzj <13631628924@163.com>
2020-03-07 12:23:20 +0800

    tree table

---
+++ com/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main extends JDialog {
+
+    public static void main(String[] args) {
+	// write your code here
+        JPanel j = new JPanel();
+
+    }
+}
